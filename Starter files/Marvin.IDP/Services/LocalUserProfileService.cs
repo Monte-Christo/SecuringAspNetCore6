4c7f171ef0af04718a88f159a1edc99e70289317
Edgar Knapp <Edgar.Knapp@simcorp.com>
2023-03-28 16:50:25 +0200

    add user claims from db

---
+++ Starter files/Marvin.IDP/Services/LocalUserProfileService.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+
+namespace Marvin.IDP.Services;
+
+public class LocalUserProfileService : IProfileService
+{
+  private readonly ILocalUserService _localUserService;
+
+  public LocalUserProfileService(ILocalUserService localUserService)
+  {
+    _localUserService = localUserService;
+  }
+  public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+  {
+    var subjectId = context.Subject.GetSubjectId();
+    var claims = (await _localUserService.GetUserClaimsBySubjectAsync(subjectId)).ToList();
+    context.AddRequestedClaims(claims.Select(c => new Claim (c.Type, c.Value)));
+  }
+
+  public async Task IsActiveAsync(IsActiveContext context)
+  {
+    var subjectId = context.Subject.GetSubjectId();
+    context.IsActive = await _localUserService.IsUserActive(subjectId);
+  }
+}
